9175244bf2c14c7c852263e0bb87f595a28cc1f2
Richard <richard@stromer.org>
2020-03-24 11:52:15 +0100

    setup aws sdk

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
+import aws from "aws-sdk";
+
+aws.config.update({
+  accessKeyId: process.env.EC2_ACCESS_KEY_ID,
+  secretAccessKey: process.env.EC2_SECRET_ACCESS_KEY,
+  region: process.env.EC2_REGION,
+});
+
 export default (_, res) => res.status(200).json({ message: "👋" });
