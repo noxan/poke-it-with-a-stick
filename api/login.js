fecf3e36382ad897f8537875221e7c6f22e22d28
Richard <richard@stromer.org>
2020-03-24 16:50:06 +0100

    add missing path info to cookie

--- api/login.js
+++ api/login.js
@@ -4,7 +4,10 @@ export default async (req, res) => {
   const { password } = JSON.parse(req.body);
 
   if (password === process.env.APP_PASSWORD) {
-    res.setHeader("Set-Cookie", cookie.serialize("auth", password));
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("auth", password, { path: "/" }),
+    );
     res.status(200).json({ message: "✅" });
   } else {
     res.status(401).json({ message: "❌" });
