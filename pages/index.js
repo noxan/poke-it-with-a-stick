359114762d2617b8e2c6b901ff95b908a5e10cc5
Richard <richard@stromer.org>
2020-03-24 16:03:12 +0100

    rename upper case keys from api in front-end to lowercase

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import apiUrl from "../utils/api-url";
 const startAction = async () => await fetch(apiUrl("/api/start"));
 const stopAction = async () => await fetch(apiUrl("/api/stop"));
 
-const Index = ({ Code, Name }) => (
+const Index = ({ code, name }) => (
   <div>
     <h1>Poke It With A Stick</h1>
     <p>
-      {Name} (code {Code})
+      {name} (code {code})
     </p>
     <p>
       <button onClick={startAction}>Start</button>
