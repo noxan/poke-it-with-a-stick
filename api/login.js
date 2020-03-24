18f25fffaf37b20f4ba44d89cdee0847078763d7
Richard <richard@stromer.org>
2020-03-24 16:45:58 +0100

    set cookie on successful auth

--- api/login.js
+++ api/login.js
@@ -1,9 +1,10 @@
-import { setCookie } from "nookies";
+import cookie from "cookie";
 
 export default async (req, res) => {
   const { password } = JSON.parse(req.body);
 
   if (password === process.env.APP_PASSWORD) {
+    res.setHeader("Set-Cookie", cookie.serialize("auth", password));
     res.status(200).json({ message: "✅" });
   } else {
     res.status(401).json({ message: "❌" });
