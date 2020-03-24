ba0554a72ce6df271135b3bc89632ca65347f076
Richard <richard@stromer.org>
2020-03-24 13:00:21 +0100

    fetch server status from api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-const Index = () => (
+import fetch from "isomorphic-unfetch";
+
+import apiUrl from "../utils/api-url";
+
+const Index = ({ Code, Name }) => (
   <div>
     <p>Poke It With A Stick</p>
+    {Name} (code {Code})
   </div>
 );
+
+Index.getInitialProps = async ({ req }) => {
+  const res = await fetch(apiUrl("/api/status", req));
+  return await res.json();
+};
 
 export default Index;
