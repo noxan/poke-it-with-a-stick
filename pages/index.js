d2055ba0979813c39033d917dd6a87634f911472
Richard <richard@stromer.org>
2020-03-24 13:06:09 +0100

    add simple start and stop actions as buttons

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import fetch from "isomorphic-unfetch";
 
 import apiUrl from "../utils/api-url";
 
+const startAction = async () => await fetch(apiUrl("/api/start"));
+const stopAction = async () => await fetch(apiUrl("/api/stop"));
+
 const Index = ({ Code, Name }) => (
   <div>
     <h1>Poke It With A Stick</h1>
     <p>
       {Name} (code {Code})
+    </p>
+    <p>
+      <button onClick={startAction}>Start</button>
+      <button onClick={stopAction}>Stop</button>
     </p>
   </div>
 );
